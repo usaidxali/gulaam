fc21b80ac0135642bd7bd0634b16f336d063695f
usaidxali <usaidxali@gmail.com>
2026-07-30 16:39:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.status(200).send('Bot Alive!');
-});
-
-app.listen(port, '0.0.0.0', () => {
-    console.log(`🌐 Web Server active on port ${port}`);
-});
-
 const { 
     default: makeWASocket, 
     useMultiFileAuthState, 
